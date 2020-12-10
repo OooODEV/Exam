4ee3b00f8c7e47dbcd61e037624ea9b62e0ec77d
OooODEV <youranus113@gmail.com>
2020-12-10 12:01:56 +0200

    1
    1

---
+++ Exam/Exercise 2/Exercise 2/PlayerItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Exercise_2
+{
+    class PlayerItem : IMedia
+    {
+        string DIRFiles = @"C:\Users\igyan\OneDrive\Рабочий стол\Exam\Exam\Exercise 2\Media";
+
+        public void GETPath()
+        {
+            List<string> files = new List<string>();
+            String dir = @"C:\Users\igyan\OneDrive\Рабочий стол\Exam\Exam\Exercise 2\Media";
+
+            foreach (string j in Directory.GetFiles(dir, "*.*"))
+            {
+                files.Add(j);
+            } 
+            
+            foreach (string d in files)
+            {
+                Console.WriteLine(d);
+            }
+            Console.ReadKey();
+        }
+    }
+}
